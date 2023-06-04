69c41f71060c2e495fcfbbdadf5e51b2f52289b6
paul practices programming <85654508+p-oca@users.noreply.github.com>
2023-06-05 02:40:35 +1000

    Update TwoSumSln.cpp
    Added comments

--- TwoSumSln.cpp
+++ TwoSumSln.cpp
@@ -8,10 +8,10 @@ public:
         while(!success){
             for(unsigned int i = 0; i < nums.size(); i++){
                 for(unsigned int j = 0; j < nums.size(); j++){
-                    if(i == j){
+                    if(i == j){ // if the two iterators reference the same element
                         // do nothing
                     } else {
-                        if (nums[i] + nums[j] == target){
+                        if (nums[i] + nums[j] == target){ // check if their sum reaches target
                             success = true;
                             return {int(i), int(j)};
                         }
@@ -22,14 +22,14 @@ public:
         // *** end of brute force method ***
 
         // *** optimal method ***
-        unordered_map<int, int> mp;
-        for (int i = 0; i < nums.size(); i++) {
-            if (mp.find(target - nums[i]) == mp.end())
-                mp[nums[i]] = i;
+        unordered_map<int, int> mp; // initialise an unordered map
+        for (int i = 0; i < nums.size(); i++) { // iterate through the array
+            if (mp.find(target - nums[i]) == mp.end()) // check if the array contains the complement of the considered element is in the map
+                mp[nums[i]] = i; // store index of array as key
             else
-                return { mp[target - nums[i]], i };
+                return { mp[target - nums[i]], i }; // return complement's index and current element
         }
-        return{ -1, -1 };
+        return{ -1, -1 }; // if no solution found
         // *** end of optimal method ***
     }
 };
